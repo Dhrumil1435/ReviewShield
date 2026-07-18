cdd2d166029eed9b3d86ea1020991c38453dadca
Dhrumil <ranadhrumil1435@gmail.com>
2026-07-18 17:31:55 +0530

    Initial project setup

---
+++ test_setup.py
@@ -0,0 +1,4 @@
+import nltk, spacy, textstat
+from vaderSentiment.vaderSentiment import SentimentIntensityAnalyzer
+import sklearn, pandas, numpy, streamlit
+print("All libraries imported successfully")
